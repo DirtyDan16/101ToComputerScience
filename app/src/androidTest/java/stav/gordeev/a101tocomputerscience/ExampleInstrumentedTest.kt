981b311015a2915d47b9e1ee2db3b7645ad584e5
DirtyDan16 <138337745+DirtyDan16@users.noreply.github.com>
2025-05-12 22:26:02 +0300

    The start of the app

---
+++ app/src/androidTest/java/stav/gordeev/a101tocomputerscience/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package stav.gordeev.a101tocomputerscience
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("stav.gordeev.a101tocomputerscience", appContext.packageName)
+    }
+}
