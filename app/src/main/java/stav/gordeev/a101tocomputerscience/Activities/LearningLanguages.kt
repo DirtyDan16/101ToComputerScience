b7218dbd6e183703c886692bccc12f33392a487e
DirtyDan16 <138337745+DirtyDan16@users.noreply.github.com>
2025-05-19 01:59:27 +0300

    Added the Learning To Code activity Menu - this activity explains what lessons it'll be teaching, as well as buttons leading to each lesson and lesson

--- app/src/main/java/stav/gordeev/a101tocomputerscience/Activities/LearningLanguages.kt
+++ app/src/main/java/stav/gordeev/a101tocomputerscience/Activities/LearningLanguages.kt
@@ -1,6 +1,8 @@
 package stav.gordeev.a101tocomputerscience.Activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +10,16 @@ import androidx.core.view.WindowInsetsCompat
 import stav.gordeev.a101tocomputerscience.R
 
 class LearningLanguages : AppCompatActivity() {
+    // Intents to sub-activities
+    lateinit var intentToLessonDataTypes: Intent
+    lateinit var intentToLessonIfStatements: Intent
+    lateinit var intentToLessonForLoops: Intent
+
+    // ImageButtons that lead to each sub-activity (ie the lessons)
+    lateinit var bToLessonDataTypes: ImageButton
+    lateinit var bToLessonIfStatements: ImageButton
+    lateinit var bToLessonForLoops: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +29,31 @@ class LearningLanguages : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initializeIntents()
+        initializeImageButtons()
+    }
+
+    private fun initializeIntents() {
+        intentToLessonDataTypes = Intent(this, LessonDataTypes::class.java)
+        intentToLessonIfStatements = Intent(this, LessonIfStatements::class.java)
+        intentToLessonForLoops = Intent(this, LessonForLoops::class.java)
+    }
+
+    private fun initializeImageButtons() {
+        bToLessonDataTypes = findViewById(R.id.bToLessonDataTypes)
+        bToLessonIfStatements = findViewById(R.id.bToLessonIfStatements)
+        bToLessonForLoops = findViewById(R.id.bToLessonForLoops)
+
+        bToLessonDataTypes.setOnClickListener {
+            startActivity(intentToLessonDataTypes)
+        }
+        bToLessonIfStatements.setOnClickListener {
+            startActivity(intentToLessonIfStatements)
+        }
+        bToLessonForLoops.setOnClickListener {
+            startActivity(intentToLessonForLoops)
+        }
+
     }
 }
