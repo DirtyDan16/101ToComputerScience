fdebc1ffeb8e26d5ea0abdc2161c3539ed7dfdcd
DirtyDan16 <138337745+DirtyDan16@users.noreply.github.com>
2025-05-31 01:41:56 +0300

    In the Menu Activity gave each button a click sound

--- app/src/main/java/stav/gordeev/a101tocomputerscience/Activities/Menu.kt
+++ app/src/main/java/stav/gordeev/a101tocomputerscience/Activities/Menu.kt
@@ -1,6 +1,7 @@
 package stav.gordeev.a101tocomputerscience.Activities
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
@@ -28,12 +29,12 @@ class Menu : AppCompatActivity() {
         }
 
         initVar()
-        initLinkingButtonsToTheirActivities()
-
+        // Set click listeners
+        setButtonListeners()
     }
 
     /**
-     * inits all the variables which includes Intents, Buttons
+     * inits all the variables to their respective view component
      */
     private fun initVar() {
         b_to_learning_languages = findViewById(R.id.b_to_learning_languages)
@@ -44,28 +45,61 @@ class Menu : AppCompatActivity() {
     }
 
     /**
-     * This method initializes the button icons to their respective activities
+     * Sets the click listeners for the buttons. upon clicking the button, it gives each button a click sound and also makes it navigate to a different activity, depending on the button's id.
      */
-    private fun initLinkingButtonsToTheirActivities() {
-        b_to_learning_languages.setOnClickListener {
-            val intentToLearningLanguages = Intent(this, LearningLanguages::class.java)
-            startActivity(intentToLearningLanguages)
+    private fun setButtonListeners() {
+        b_to_history.setOnClickListener { playClickSoundAndNavigate(b_to_history) }
+        b_to_learning_languages.setOnClickListener { playClickSoundAndNavigate(b_to_learning_languages) }
+        b_to_career_path.setOnClickListener { playClickSoundAndNavigate(b_to_career_path) }
+        b_to_extras.setOnClickListener { playClickSoundAndNavigate(b_to_extras) }
+        b_to_motives.setOnClickListener { playClickSoundAndNavigate(b_to_motives) }
+    }
+
+
+    /**
+     * Gives the given button a click sound.
+     */
+    private fun playClickSound() {
+        val mpButtonPressed = MediaPlayer.create(this, R.raw.sfx_button_being_clicked)
+
+        // Set a listener to release the MediaPlayer when playback is complete.
+        mpButtonPressed?.setOnCompletionListener { player: MediaPlayer ->
+            player.release()
         }
-        b_to_extras.setOnClickListener {
-            val intentToExtras =Intent(this, Extras::class.java)
-            startActivity(intentToExtras)
-        }
-        b_to_history.setOnClickListener {
-            val intentToHistory =Intent(this, HistoryOfLanguages::class.java)
-            startActivity(intentToHistory)
-        }
-        b_to_motives.setOnClickListener {
-            val intentToMotives = Intent(this, MotivesForApp::class.java)
-            startActivity(intentToMotives)
-        }
-        b_to_career_path.setOnClickListener {
-            val intentToCareerPath = Intent(this, CareerPath::class.java)
-            startActivity(intentToCareerPath)
+
+        // Start playing.
+        mpButtonPressed?.start()
+    }
+
+    /**
+     * Makes the given button have a click sound and also make it navigate to a different activity, depending on the button's id.
+     */
+    private fun playClickSoundAndNavigate(button: ImageButton) {
+        // give the button a click sound
+        playClickSound()
+        // make the button the capability to navigate to a different activity, depending on the button's id
+        when (button.id) {
+            R.id.b_to_history -> {
+                val intentToLearningLanguages = Intent(this, LearningLanguages::class.java)
+                startActivity(intentToLearningLanguages)
+            }
+            R.id.b_to_learning_languages -> {
+                val intentToExtras =Intent(this, Extras::class.java)
+                startActivity(intentToExtras)
+            }
+            R.id.b_to_career_path -> {
+                val intentToCareerPath = Intent(this, CareerPath::class.java)
+                startActivity(intentToCareerPath)
+            }
+            R.id.b_to_motives -> {
+                val intentToMotives = Intent(this, MotivesForApp::class.java)
+                startActivity(intentToMotives)
+            }
+            R.id.b_to_extras -> {
+                val intentToExtras = Intent(this, Extras::class.java)
+                startActivity(intentToExtras)
+            }
         }
     }
 }
+
