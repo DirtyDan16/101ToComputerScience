1c530cb38b6fe5bcbf6bb09d06cafe5571c8deea
DirtyDan16 <138337745+DirtyDan16@users.noreply.github.com>
2025-05-30 22:55:05 +0300

    Added the History Activity - this activity summarizes the development of Coding throughout the years and talks about different computer languages that came up at different periods.

--- app/src/main/java/stav/gordeev/a101tocomputerscience/Activities/HistoryOfLanguages.kt
+++ app/src/main/java/stav/gordeev/a101tocomputerscience/Activities/HistoryOfLanguages.kt
@@ -1,29 +1,25 @@
 package stav.gordeev.a101tocomputerscience.Activities
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import stav.gordeev.a101tocomputerscience.R
 import stav.gordeev.a101tocomputerscience.databinding.ActivityHistoryOfLanguagesBinding
 
 class HistoryOfLanguages : AppCompatActivity() {
-
-    private lateinit var binding: ActivityHistoryOfLanguagesBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityHistoryOfLanguagesBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(findViewById(R.id.toolbar))
-        binding.toolbarLayout.title = title
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_history_of_languages)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
